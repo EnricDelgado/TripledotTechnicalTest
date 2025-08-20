e7fa6104dd1bbf94ae6b42b0cd68e43c661d3b27
RunningLate <enricdelgadoclaramunt@gmail.com>
2025-08-20 10:38:23 +0200

    Added actor meshes from mainscreen

--- Assets/Scripts/UI/Layout/AnimatedLayoutElement.cs
+++ Assets/Scripts/UI/Layout/AnimatedLayoutElement.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
@@ -6,45 +7,128 @@ using UnityEngine.UI;
 
 public class AnimatedLayoutElement : TweenedElement
 {
-    [SerializeField] private float _resizeDuration = 0.18f;
-    [SerializeField] private LeanTweenType _resizeCurve = LeanTweenType.easeInBounce;
-
     private RectTransform _rectTransform;
     private LayoutElement _layoutElement;
-    private int _widthActionID;
 
     public LayoutElement Element => _layoutElement;
 
-
+    
     private void Awake()
     {
         _layoutElement = GetComponent<LayoutElement>();
         _rectTransform = GetComponent<RectTransform>();
     }
 
-    public UniTask TweenWidthAsync(float to, CancellationToken cancelToken)
+    public async override UniTask PlayClip(TweenClip clip, CancellationToken cancelToken)
     {
-        CancelChannel(TweenChannel.LayoutWidth);
+        var tasks = new List<UniTask>();
+
+        if (clip.useMove) throw new NotImplementedException();
+
+        if (clip.useScale)
+        {
+            if (clip.separateAxis)
+            {
+                bool widthAxis = (clip.effectAxis == EffectAxis.X);
+
+                float from = widthAxis
+                    ? (_layoutElement.preferredWidth  > 0 ? _layoutElement.preferredWidth  : _rectTransform.rect.width)
+                    : (_layoutElement.preferredHeight > 0 ? _layoutElement.preferredHeight : _rectTransform.rect.height);
+
+                float to = 0;
+
+                switch (clip.scaleToMode)
+                {
+                    case ToModeScale.Absolute:
+                        to = widthAxis ? clip.scaleTo.x : clip.scaleTo.y;
+                        break;
+                    case ToModeScale.ByOffset:
+                        to = from + (widthAxis ? clip.scaleDelta.x : clip.scaleDelta.y);
+                        break;
+                    case ToModeScale.MultiplyBy:
+                        to = from * clip.scaleMultiply;
+                        break;
+                }
+
+                tasks.Add(TweenAxis(clip.effectAxis, from, to, clip.scaleDuration, clip.scaleEase, cancelToken));
+            }
+            else
+            {
+                Vector3 from = _rectTransform.localScale;
+                Vector3 to = from;
+
+                switch (clip.scaleToMode)
+                {
+                    case ToModeScale.Absolute:
+                        to = clip.scaleTo;
+                        break;
+                    case ToModeScale.ByOffset:
+                        to = from + clip.scaleDelta;
+                        break;
+                    case ToModeScale.MultiplyBy:
+                        to = from * clip.scaleMultiply;
+                        break;
+                }
+
+                tasks.Add(TweenScale(from, to, clip.scaleDuration, clip.scaleEase, cancelToken));
+            }
+        }
+
+        if (clip.useAlpha) throw new NotImplementedException();
+
+        await UniTask.WhenAll(tasks);
+    }
+
+
+    private UniTask TweenAxis(EffectAxis axis, float from, float to, float duration, LeanTweenType ease, CancellationToken cancelToken)
+    {
+        CancelChannel(TweenChannel.Scale);
         var completionSource = new UniTaskCompletionSource();
         
-        float from = _layoutElement.preferredWidth > 0f ? _layoutElement.preferredWidth : _rectTransform.rect.width;
-
-        _widthActionID = LeanTween.value(gameObject, from, to, _resizeDuration)
-            .setEase(_resizeCurve)
+        int actionID = LeanTween.value(gameObject, from, to, duration)
+            .setEase(ease)
             .setIgnoreTimeScale(true)
             .setOnUpdate((value) =>
             {
-                _layoutElement.preferredWidth = value;
+                if(axis == EffectAxis.X) 
+                    _layoutElement.preferredWidth = value;
+                else
+                    _layoutElement.preferredHeight = value;
+                
                 if (transform.parent is RectTransform parent)
                     LayoutRebuilder.MarkLayoutForRebuild(parent);
             })
             .setOnComplete(() =>
             {
-                ClearChannel(TweenChannel.LayoutWidth);
+                ClearChannel(TweenChannel.Scale);
+                completionSource.TrySetResult();
+            })
+            .id;
+        
+        FillChannel(TweenChannel.Scale, actionID);
+        RegisterChannelCancellationToken(cancelToken, TweenChannel.Scale, actionID, completionSource);
+        return completionSource.Task;
+    }
+    
+    private UniTask TweenScale(Vector3 from, Vector3 to, float duration, LeanTweenType ease, CancellationToken cancelToken)
+    {
+        CancelChannel(TweenChannel.Scale);
+        var completionSource = new UniTaskCompletionSource();
+        
+        _rectTransform.localScale = from;
+        
+        int actionID = LeanTween.scale(gameObject, to, duration)
+            .setEase(ease)
+            .setIgnoreTimeScale(true)
+            .setOnComplete(() =>
+            {
+                ClearChannel(TweenChannel.Scale);
                 completionSource.TrySetResult();
             }).id;
+        
+        FillChannel(TweenChannel.Scale, actionID);
 
-        RegisterChannelCancellationToken(cancelToken, TweenChannel.LayoutWidth, completionSource);
+        RegisterChannelCancellationToken(cancelToken, TweenChannel.Scale, actionID, completionSource);
         
         return completionSource.Task;
     }
