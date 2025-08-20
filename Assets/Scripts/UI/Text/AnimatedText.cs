5484c3aeac54ee1ff95b5a5a1c6b4d4d04603177
RunningLate <enricdelgadoclaramunt@gmail.com>
2025-08-20 10:48:54 +0200

    improved AnimatedText class

--- Assets/Scripts/UI/Text/AnimatedText.cs
+++ Assets/Scripts/UI/Text/AnimatedText.cs
@@ -1,25 +1,47 @@
+using System;
+using System.Collections.Generic;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 using TMPro;
 
+[RequireComponent(typeof(TextMeshProUGUI))]
 public class AnimatedText : TweenedElement
 {
+    // this could be cached on awake + required component and removed from here
     [SerializeField] private TextMeshProUGUI _text;
-    [SerializeField] private float _duration = .2f;
-    [SerializeField] private LeanTweenType _curve = LeanTweenType.easeInOutCubic;
 
-    private int _alphaTweenId = -1;
-
-    public UniTask TweenTextVisibility(float to, CancellationToken cancelToken)
+    
+    public override async UniTask PlayClip(TweenClip clip, CancellationToken cancelToken)
     {
-        CancelChannel(TweenChannel.Text);
+        var tasks = new List<UniTask>();
+
+        if (clip.useMove)
+        {
+            throw new NotImplementedException();
+        }
+
+        if (clip.useScale)
+        {
+            throw new NotImplementedException();
+        }
+
+        if (clip.useAlpha)
+        {
+            float from = (clip.alphaFrom == FromMode.Current) ? _text.color.a : clip.initalAlpha;
+            tasks.Add(TweenTextAlpha(from, clip.alphaTo, clip.alphaDuration, clip.alphaEase, cancelToken));
+        }
+        
+        await UniTask.WhenAll(tasks);
+    }
+
+    public UniTask TweenTextAlpha(float from, float to, float duration, LeanTweenType ease, CancellationToken cancelToken)
+    {
+        CancelChannel(TweenChannel.Alpha);
         var completionSource = new UniTaskCompletionSource();
-
-        float from = _text.color.a;
-
-        _alphaTweenId = LeanTween.value(gameObject, from, to, _duration)
-            .setEase(_curve)
+        
+        int actionID = LeanTween.value(gameObject, from, to, duration)
+            .setEase(ease)
             .setIgnoreTimeScale(true)
             .setOnUpdate((a) =>
             {
@@ -27,13 +49,13 @@ public class AnimatedText : TweenedElement
             })
             .setOnComplete(() =>
             {
-                ClearChannel(TweenChannel.Text);
+                ClearChannel(TweenChannel.Alpha);
                 completionSource.TrySetResult();
             }).id;
         
-        FillChannel(TweenChannel.Text, _alphaTweenId);
+        FillChannel(TweenChannel.Alpha, actionID);
         
-        RegisterChannelCancellationToken(cancelToken, TweenChannel.Text, completionSource);
+        RegisterChannelCancellationToken(cancelToken, TweenChannel.Alpha, actionID, completionSource);
 
         return completionSource.Task;
     }
