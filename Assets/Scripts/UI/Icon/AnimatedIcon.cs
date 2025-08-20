0dd4f1a293aab602aa5769a9e1524efeb883c351
RunningLate <enricdelgadoclaramunt@gmail.com>
2025-08-20 10:50:25 +0200

    refactored animated icon to use tween clips

--- Assets/Scripts/UI/Icon/AnimatedIcon.cs
+++ Assets/Scripts/UI/Icon/AnimatedIcon.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 using Cysharp.Threading.Tasks;
+using Random = UnityEngine.Random;
 
 public class AnimatedIcon : TweenedElement
 {
@@ -10,26 +12,8 @@ public class AnimatedIcon : TweenedElement
     [SerializeField] private Image _icon;
     [SerializeField] private RectTransform _iconTransform;
     
-    [Header("Animation Settings")] 
-    [Header("Position")] 
-    [SerializeField] private float _movementDuration = .3f;
-    [SerializeField] private LeanTweenType _movementEase = LeanTweenType.easeOutBack;
-    
-    [Header("Scale")] 
-    [SerializeField] private float _resizeDuration = 0.3f;
-    [SerializeField] private LeanTweenType _resizeCurve = LeanTweenType.easeInOutCubic;
-    
-    [Header("Color")] 
-    [SerializeField] private float _alphaDuration = .3f;
-    [SerializeField] private LeanTweenType _alphaEase = LeanTweenType.easeOutBack;
-
-    private int _tweenId = -1;
-
     private Vector3 _initialPosition;
     private Vector3 _initialScale;
-    private float _initialAlpha;
-    
-    public RectTransform RectTransform => _iconTransform;
 
 
     private void Awake()
@@ -39,12 +23,62 @@ public class AnimatedIcon : TweenedElement
 
         _initialScale = _iconTransform.localScale;
         _initialPosition = _iconTransform.localPosition;
-        _initialAlpha = _icon.color.a;
     }
-
-    private void OnDisable() => CancelAllChannels();
     
-    #region Scale Tween Logic
+    public override async UniTask PlayClip(TweenClip clip, CancellationToken cancelToken)
+    {
+        var tasks = new List<UniTask>(3);
+
+        if (clip.useScale)
+        {
+            var from = (clip.scaleFrom == FromMode.Current) ? _iconTransform.localScale : _initialScale;
+            Vector3 to;
+            
+            switch (clip.scaleToMode)
+            {
+                case ToModeScale.Absolute:   
+                    to = clip.scaleTo; 
+                    break;
+                case ToModeScale.MultiplyBy: 
+                    to = from * clip.scaleMultiply; 
+                    break;
+                case ToModeScale.ByOffset:   
+                    to = from + clip.scaleDelta; 
+                    break;
+                default:                     
+                    to = from; 
+                    break;
+            }
+
+            tasks.Add(TweenIconScaleFrom(from, to, clip.scaleDuration, clip.scaleEase, cancelToken));
+        }
+
+        if (clip.useMove)
+        {
+            var from = (clip.moveFrom == FromMode.Current) ? _iconTransform.localPosition : _initialPosition;
+            Vector3 to = (clip.moveToMode == ToModePosition.Absolute) ? clip.moveTo : from + clip.moveOffset;
+
+            tasks.Add(TweenIconPositionFrom(from, to, clip.moveDuration, clip.moveEase, cancelToken));
+        }
+
+        if (clip.useAlpha)
+        {
+            float from = (clip.alphaFrom == FromMode.Current) ? _icon.color.a : clip.initalAlpha;
+
+            tasks.Add(TweenIconAlphaFrom(from, clip.alphaTo, clip.alphaDuration, clip.alphaEase, cancelToken));
+        }
+
+        if (clip.useShake)
+            tasks.Add(TweenShake(clip.shakeDuration, clip.shakeMagnitude, clip.shakeFrequency, clip.shakeEase, cancelToken));
+
+        if (clip.useJellyBounce)
+            tasks.Add(TweenJellyBounce(clip.jellyBounceDuration, clip.jellyOvershootScale, clip.jellyBounceEase, cancelToken));
+        
+        
+
+        await UniTask.WhenAll(tasks);
+    }
+    
     private UniTask TweenIconScaleFrom(Vector3 from, Vector3 to, float duration, LeanTweenType easeType, CancellationToken cancelToken)
     {
         CancelChannel(TweenChannel.Scale);
@@ -64,16 +98,11 @@ public class AnimatedIcon : TweenedElement
         
         FillChannel(TweenChannel.Scale, actionID);
         
-        RegisterChannelCancellationToken(cancelToken, TweenChannel.Scale, completionSource);
+        RegisterChannelCancellationToken(cancelToken, TweenChannel.Scale, actionID, completionSource);
         
         return completionSource.Task;
     }
     
-    public async UniTask TweenIconScale(Vector3 to, CancellationToken cancelToken) => await TweenIconScaleFrom(_iconTransform.localScale, to, _resizeDuration, _resizeCurve, cancelToken);
-    public async UniTaskVoid ResetIconScale(CancellationToken cancellationToken) => await TweenIconScale(_initialScale, cancellationToken);
-    #endregion
-    
-    #region Movement Tween Logic
     private UniTask TweenIconPositionFrom(Vector3 from, Vector3 to, float duration, LeanTweenType easeType, CancellationToken cancelToken)
     {
         CancelChannel(TweenChannel.Position);
@@ -93,18 +122,12 @@ public class AnimatedIcon : TweenedElement
             .id;
         
         FillChannel(TweenChannel.Position, actionID);
-        RegisterChannelCancellationToken(cancelToken, TweenChannel.Position, completionSource);
+        RegisterChannelCancellationToken(cancelToken, TweenChannel.Position, actionID, completionSource);
         
         return completionSource.Task;
     }
     
-    public async UniTask TweenIconPosition(Vector3 to, CancellationToken cancelToken) => await TweenIconPositionFrom(_iconTransform.localPosition, to, _movementDuration, _movementEase, cancelToken);
-    public async UniTask ResetIconPosition(CancellationToken cancelToken) => await TweenIconPosition(_initialPosition, cancelToken);
-    #endregion
-    
-    #region Color Tween Logic
-    private UniTask TweenIconAlphaFrom(float from, float to, float duration, LeanTweenType easeType,
-        CancellationToken cancelToken)
+    private UniTask TweenIconAlphaFrom(float from, float to, float duration, LeanTweenType easeType, CancellationToken cancelToken)
     {
         CancelChannel(TweenChannel.Alpha);
 
@@ -130,60 +153,85 @@ public class AnimatedIcon : TweenedElement
             }).id;
         
         FillChannel(TweenChannel.Alpha, actionID);
-        RegisterChannelCancellationToken(cancelToken, TweenChannel.Alpha, completionSource);
+        RegisterChannelCancellationToken(cancelToken, TweenChannel.Alpha, actionID, completionSource);
         
         return completionSource.Task;
     }
-    
-    public async UniTask TweenIconAlpha(float to, CancellationToken cancelToken) => await TweenIconAlphaFrom(_icon.color.a, to, _alphaDuration, _alphaEase, cancelToken);
-    public async UniTask ResetIconAlpha(CancellationToken cancelToken) => await TweenIconAlpha(_initialAlpha, cancelToken);
-    #endregion
-    
-    #region Clip Tween Logic
-    public async UniTask PlayClip(IconTweenClip clip, CancellationToken ct)
+
+    private UniTask TweenShake(float duration, float magnitude, float frequency, LeanTweenType easeType, CancellationToken cancelToken)
     {
-        var tasks = new List<UniTask>(3);
+        CancelChannel(TweenChannel.Shake);
 
-        if (clip.useScale)
-        {
-            var from = (clip.scaleFrom == FromMode.Current) ? _iconTransform.localScale : _initialScale;
-            Vector3 to;
-            switch (clip.scaleToMode)
+        var completionSource = new UniTaskCompletionSource();
+
+        Vector2 originalPos = _iconTransform.anchoredPosition;
+
+        float seedX = Random.Range(0f, 999f);
+        float seedY = seedX + 100.123f;
+
+        int actionID = LeanTween.value(gameObject, 0f, 1f, duration)
+            .setEase(easeType)
+            .setIgnoreTimeScale(true)
+            .setOnUpdate((float t) =>
             {
-                case ToModeScale.Absolute:   
-                    to = clip.scaleTo; 
-                    break;
-                case ToModeScale.MultiplyBy: 
-                    to = from * clip.scaleMultiply; 
-                    break;
-                case ToModeScale.ByOffset:   
-                    to = from + clip.scaleDelta; 
-                    break;
-                default:                     
-                    to = from; 
-                    break;
-            }
+                float time = Time.unscaledTime;
+                float nX = Mathf.PerlinNoise(seedX, time * frequency) * 2f - 1f;
+                float nY = Mathf.PerlinNoise(seedY, time * frequency) * 2f - 1f;
 
-            tasks.Add(TweenIconScaleFrom(from, to, clip.scaleDuration, clip.scaleEase, ct));
-        }
+                float envelope = 1f - Mathf.Abs(2f * t - 1f);
+                float amt = magnitude * (0.5f + 0.5f * envelope);
 
-        if (clip.useMove)
-        {
-            var from = (clip.moveFrom == FromMode.Current) ? _iconTransform.localPosition : _initialPosition;
-            Vector3 to = (clip.moveToMode == ToModePosition.Absolute) ? clip.moveTo : from + clip.moveOffset;
+                _iconTransform.anchoredPosition = originalPos + new Vector2(nX, nY) * amt;
+            })
+            .setOnComplete(() =>
+            {
+                _iconTransform.anchoredPosition = originalPos;
 
-            tasks.Add(TweenIconPositionFrom(from, to, clip.moveDuration, clip.moveEase, ct));
-        }
+                ClearChannel(TweenChannel.Shake);
+                completionSource.TrySetResult();
+            }).id;
 
-        if (clip.useAlpha)
-        {
-            float from = (clip.alphaFrom == FromMode.Current) ? _icon.color.a : _initialAlpha;
-            float to = (clip.alphaToMode == ToModeAlpha.Absolute) ? clip.alphaTo : from + clip.alphaDelta;
+        FillChannel(TweenChannel.Shake, actionID);
+        RegisterChannelCancellationToken(cancelToken, TweenChannel.Shake, actionID, completionSource);
 
-            tasks.Add(TweenIconAlphaFrom(from, to, clip.alphaDuration, clip.alphaEase, ct));
-        }
-
-        await UniTask.WhenAll(tasks);
+        return completionSource.Task;
     }
-    #endregion
+    
+    private UniTask TweenJellyBounce(float duration, float overshootScale, LeanTweenType easeType, CancellationToken cancelToken)
+    {
+        CancelChannel(TweenChannel.Jelly);
+
+        var completionSource = new UniTaskCompletionSource();
+
+        Vector3 originalScale = _iconTransform.localScale;
+        
+        float overshootRange = overshootScale * 0.33f; 
+        float oversoot = Random.Range(overshootScale -= overshootRange, overshootScale += overshootRange);
+
+        int actionID = LeanTween.value(gameObject, 0f, 1f, duration)
+            .setEase(easeType)
+            .setIgnoreTimeScale(true)
+            .setOnUpdate((float t) =>
+            {
+                float squash = Mathf.Sin(t * Mathf.PI);
+                float stretch = Mathf.Cos(t * Mathf.PI);
+
+                float scaleX = Mathf.Lerp(originalScale.x, originalScale.x * oversoot, squash);
+                float scaleY = Mathf.Lerp(originalScale.y, originalScale.y / oversoot, stretch);
+
+                _iconTransform.localScale = new Vector3(scaleX, scaleY, originalScale.z);
+            })
+            .setOnComplete(() =>
+            {
+                _iconTransform.localScale = originalScale;
+
+                ClearChannel(TweenChannel.Jelly);
+                completionSource.TrySetResult();
+            }).id;
+
+        FillChannel(TweenChannel.Jelly, actionID);
+        RegisterChannelCancellationToken(cancelToken, TweenChannel.Jelly, actionID, completionSource);
+
+        return completionSource.Task;
+    }
 }
